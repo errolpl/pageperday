a937f279488d3a4b82d34013cae5d2b6009bbbf8
Errol <errolpierrelouis@gmail.com>
2024-04-02 16:42:45 -0400

    "complete JS logic" :wq

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const pageCount = 356
 const meetingDate = new Date("2024-04-24")
 const currentDate = new Date()
+const oneDay = 1000 * 3600 * 24
 let currentPage = 50
 
-let daysToMeeting = Math.floor((meetingDate.getTime() - currentDate.getTime())/(1000 * 3600 * 24))
+let daysToMeeting = Math.floor((meetingDate.getTime() - currentDate.getTime())/oneDay) - 1
+let pagesLeft = Math.ceil((pageCount - currentPage)/daysToMeeting)
 
-console.log(daysToMeeting)
+console.log(`Days left: ${daysToMeeting}`)
+console.log(`Pages per day needed: ${pagesLeft}`)
